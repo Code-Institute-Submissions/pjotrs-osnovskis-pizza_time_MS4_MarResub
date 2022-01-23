420b92ebfbf7acc0e2dcb0a028aa258e4b214985
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-01-23 19:35:47 +0000

    feat: Add products app and models

--- pizzatime/settings.py
+++ pizzatime/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
     # Custom Apps
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    full_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_full_name(self):
+        return self.full_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    ingredients = models.TextField()
+    price_s = models.DecimalField(max_digits=4, decimal_places=2)
+    price_m = models.DecimalField(max_digits=4, decimal_places=2)
+    price_l = models.DecimalField(max_digits=4, decimal_places=2)
+    rating = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
