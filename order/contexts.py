7f2d2f9b5c10cc27d36e1700698308869422bcd3
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-06 15:09:14 +0000

    fix: Update context processor to view items in order

--- order/contexts.py
+++ order/contexts.py
@@ -1,9 +1,29 @@
+from django.shortcuts import get_object_or_404
+from products.models import Pizza
 
 def order_contents(request):
 
     order_items = []
     total = 0
     product_count = 0
+    order = request.session.get('order', {})
+
+    for item_id, qty in order.items():
+        product = get_object_or_404(Pizza, pk=item_id)
+        if product.price_s:
+            total += qty * float(product.price_s)
+        elif product.size_m:
+            total += qty * float(product.price_m)
+        elif product.size_l:
+            total += qty * float(product.price_L)
+
+
+        product_count += qty
+        order_items.append({
+            'item_id': item_id,
+            'qty': qty,
+            'product': product,
+        })
 
     grand_total = total # will keep this simple for now, will add delivery logic later
 
--- order/views.py
+++ order/views.py
@@ -7,12 +7,15 @@ def order(request):
 def add_to_order(request, item_id):
     """ Add selected product to order"""
 
-    size = float(request.POST.get('size'))
+    quantity = int(request.POST.get('qty'))
+    price = float(request.POST.get('size'))
     redirect_url = request.POST.get('redirect_url')
     order = request.session.get('order', {})
 
-    order[item_id] = size
+    if item_id in list(order.keys()):
+        order[item_id] += quantity
+    else:
+        order[item_id] = quantity
 
     request.session['order'] = order
-    print(request.session['order'])
     return redirect(redirect_url)
