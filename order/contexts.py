35bb63e93c2e3b7f1f4cfa4b2d10560f08a5669d
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-10 18:53:07 +0000

    fix: Update contexts.py to show size, price, qty, total correctly for each product added to order.

--- order/contexts.py
+++ order/contexts.py
@@ -5,40 +5,50 @@ def order_contents(request):
 
     order_items = []
     total = 0
+    grand_total = 0
+    item_count = 0
     order = request.session.get('order', {})
+    qty = 0
+    price = 0.00
 
-    for item_id, qty in order.items():
-        product = get_object_or_404(Product, pk=item_id)
-        if product.price_s:
-            total += qty * float(product.price_s)
-        elif product.price_m:
-            total += qty * float(product.price_m)
-        elif product.price_l:
-            total += qty * float(product.price_l)
 
-        order_items.append({
-            'item_id': item_id,
-            'price': price,
-            'qty': qty,
-            'total': total,
-            'product': product,
-        })
 
-    # for item_id, price in order.items():
-    #     product = get_object_or_404(Product, pk=item_id)
-    #     if price == product.price_s:
-    #         return product.price_s
-    #     elif price == product.price_m:
-    #         return product.price_m
-    #     else:
-    #         return product.price_l
+    for item_id, item_data in order.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * price
+            item_count += item_data
+            order_items.append({
+                'item_id': item_id,
+                'qty': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, qty in item_data['items_by_size'].items():
+                if size == "s":
+                    price = product.price_s
+                elif size == "m":
+                    price = product.price_m
+                elif size == "l":
+                    price = product.price_l
 
-    grand_total = total # will keep this simple for now, will add delivery logic later
+                total = qty * price
+                grand_total += qty * price
+                item_count += qty
+                order_items.append({
+                    'item_id': item_id,
+                    'qty': qty,
+                    'size': size,
+                    'price': price,
+                    'total': total,
+                    'product': product,
+                })
 
     context = {
         'order_items': order_items,
-        'total': total,
         'grand_total': grand_total,
+        'item_count': item_count,
     }
-
+    print(f"CONTEXT: {context}")
     return context
--- order/views.py
+++ order/views.py
@@ -7,19 +7,31 @@ def order(request):
 def add_to_order(request, item_id):
     """ Add selected product to order"""
 
-    quantity = int(request.POST.get('qty'))
+    qty = int(request.POST.get('qty'))
     price = float(request.POST.get('price'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+
+    if 'size' in request.POST:
+        size = request.POST['size']
+        print(size)
     order = request.session.get('order', {})
 
-    if item_id in list(order.keys()):
-        order[item_id] += quantity
+    if size:
+        if item_id in list(order.keys()):
+            if size in order[item_id]['items_by_size'].keys():
+                order[item_id]['items_by_size'][size] += qty
+            else:
+                order[item_id]['items_by_size'][size] = qty
+        else:
+            order[item_id] = {'items_by_size': {size: qty}}
     else:
-        order[item_id] = quantity
-
-    
+        if item_id in list(order.keys()):
+            order[item_id] += qty
+        else:
+            order[item_id] = qty    
 
     # print(price)
-    print(request.POST)
+    print(f"POST: {request.POST}")
     request.session['order'] = order
     return redirect(redirect_url)
