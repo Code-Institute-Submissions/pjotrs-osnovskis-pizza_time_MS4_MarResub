5e434661acbbc70a2390a63772ec89646b7846a5
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-21 11:58:02 +0000

    feat: Add messages to order update and item remove functionality

--- order/views.py
+++ order/views.py
@@ -8,6 +8,7 @@ def order(request):
     """ A view for an order """
     return render(request, 'order/order.html')
 
+
 def add_to_order(request, item_id):
     """ Add selected product to order"""
     product = Product.objects.get(pk=item_id)
@@ -16,61 +17,72 @@ def add_to_order(request, item_id):
 
     if 'size' in request.POST:
         size = request.POST['size']
+
+    # Give plural ending in needed.
+    if qty == 1:
         ending = ""
-        if qty == 1:
-            ending = ""
-        else:
-            ending = "'s"
+    else:
+        ending = "'s"
+
+    # Give sizes an appropriate names
+    if size == "s":
+        size_name = "Small"
+    elif size == "m":
+        size_name = "Medium"
+    else:
+        size_name = "Large"
 
     order = request.session.get('order', {})
-    
-    if size:
-        if item_id in list(order.keys()):
-            if size in order[item_id]['items_by_size'].keys():
-                order[item_id]['items_by_size'][size] += qty
-                messages.success(request, f'Done! {qty} more {size} {product.display_name}{ending} added to your order.')
-            else:
-                order[item_id]['items_by_size'][size] = qty
-                messages.success(request, f'Done! {qty} {size} {product.display_name}{ending} added to your order.')
+
+    if item_id in list(order.keys()):
+        if size in order[item_id]['items_by_size'].keys():
+            order[item_id]['items_by_size'][size] += qty
+            messages.success(request, f'Perfect! {qty} more {size_name} {product.display_name}{ending} added to your order.')
         else:
-            order[item_id] = {'items_by_size': {size: qty}}
-            messages.success(request, f'Done! {qty} {size} {product.display_name}{ending} added to your order.')
+            order[item_id]['items_by_size'][size] = qty
+            messages.success(request, f'Amazing! {qty} {size_name} {product.display_name}{ending} added to your order.')
     else:
-        if item_id in list(order.keys()):
-            order[item_id] += qty
-        else:
-            order[item_id] = qty    
-            
+        order[item_id] = {'items_by_size': {size: qty}}
+        messages.success(request, f'Great! {qty} {size_name} {product.display_name}{ending} added to your order.')
 
     request.session['order'] = order
     # After submitting "Add to order", stays on the same page. Idea taken from here:
     # https://stackoverflow.com/questions/39560175/redirect-to-same-page-after-post-method-using-class-based-views
     return redirect("/products/?category=pizza")
 
+
 def change_order(request, item_id):
     """ Change selected product in the order"""
+    product = Product.objects.get(pk=item_id)
     qty = int(request.POST.get('qty'))
     size = None
 
     if 'size' in request.POST:
-        size = request.POST.get('size')
+        size = request.POST['size']
+
+    # Give plural ending in needed.
+    if qty == 1:
+        ending = ""
+    else:
+        ending = "'s"
+
+    # Give sizes an appropriate names
+    if size == "s":
+        size_name = "Small"
+    elif size == "m":
+        size_name = "Medium"
+    else:
+        size_name = "Large"
 
     order = request.session.get('order', {})
 
-  
-    if size:
-        if qty > 0:
-            order[item_id]['items_by_size'][size] = qty
-        else:
-            del order[item_id]['items_by_size'][size]
-            if not order[item_id]['items_by_size']:
-                order.pop(item_id)
+    if qty > 0:
+        order[item_id]['items_by_size'][size] = qty
+        messages.success(request, f'Updated! There is {qty} {size_name} {product.display_name}{ending} in your order now.')
     else:
-        if qty > 0:
-            order[item_id] = qty
-        else:
+        del order[item_id]['items_by_size'][size]
+        if not order[item_id]['items_by_size']:
             order.pop(item_id)
-
 
     request.session['order'] = order
     return redirect(reverse('order'))
@@ -81,19 +93,32 @@ def remove_from_order(request, item_id):
 
     try:
         size = None
+
         if 'product_size' in request.POST:
             size = request.POST['product_size']
+            qty = request.POST['product_qty']
+
+        # Give plural ending in needed.
+        if qty == 1:
+            ending = ""
+        else:
+            ending = "'s"
+
+        # Give sizes an appropriate names
+        if size == "s":
+            size_name = "Small"
+        elif size == "m":
+            size_name = "Medium"
+        else:
+            size_name = "Large"
+
         order = request.session.get('order', {})
 
-        if size:
-            del order[item_id]['items_by_size'][size]
-            if not order[item_id]['items_by_size']:
-                order.pop(item_id)
-                messages.warning(request, f'Done! {product.display_name} removed.')
-                
-        else:
+        del order[item_id]['items_by_size'][size]
+        if not order[item_id]['items_by_size']:
             order.pop(item_id)
-            
+            messages.warning(request, f'You have removed {qty} {size_name} {product.display_name}{ending} from your order.')
+
         request.session['order'] = order
         return HttpResponse(status=200)
 
