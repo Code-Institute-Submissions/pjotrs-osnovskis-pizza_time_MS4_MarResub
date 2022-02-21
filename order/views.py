9ff0f37159f8be3be472de402a2e6939589b589c
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-21 13:04:49 +0000

    feat: Show mini oreder list on adding a new item

--- order/views.py
+++ order/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import redirect, render, reverse, HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render, reverse, HttpResponse
 from django.contrib import messages
 
 from products.models import Product
+
 
 def order(request):
     """ A view for an order """
@@ -11,12 +11,9 @@ def order(request):
 
 def add_to_order(request, item_id):
     """ Add selected product to order"""
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     qty = int(request.POST.get('qty'))
-    size = None
-
-    if 'size' in request.POST:
-        size = request.POST['size']
+    size = request.POST['size']
 
     # Give plural ending in needed.
     if qty == 1:
@@ -48,17 +45,13 @@ def add_to_order(request, item_id):
     request.session['order'] = order
     # After submitting "Add to order", stays on the same page. Idea taken from here:
     # https://stackoverflow.com/questions/39560175/redirect-to-same-page-after-post-method-using-class-based-views
-    return redirect("/products/?category=pizza")
-
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
 def change_order(request, item_id):
     """ Change selected product in the order"""
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     qty = int(request.POST.get('qty'))
-    size = None
-
-    if 'size' in request.POST:
-        size = request.POST['size']
+    size = request.POST['size']
 
     # Give plural ending in needed.
     if qty == 1:
@@ -89,14 +82,11 @@ def change_order(request, item_id):
 
 def remove_from_order(request, item_id):
     """Remove the item from the shopping order"""
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
 
     try:
-        size = None
-
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-            qty = request.POST['product_qty']
+        size = request.POST['product_size']
+        qty = request.POST['product_qty']
 
         # Give plural ending in needed.
         if qty == 1:
@@ -123,4 +113,5 @@ def remove_from_order(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}.')
         return HttpResponse(status=500)
