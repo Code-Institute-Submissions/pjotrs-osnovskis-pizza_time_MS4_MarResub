bd537b7762b9d23fb7041d305fc9ea05aea3e58a
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-22 10:44:36 +0000

    feat: Add Stripe elements, css and js

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,12 @@ from django.contrib import messages
 
 from .forms import CheckoutForm
 
+import os
+if os.path.exists("env.py"):
+    import env
+
+os.environ.get("SECRET_KEY")
+
 def checkout(request):
     order = request.session.get('order', {})
 
@@ -14,6 +20,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': os.environ.get('STRIPE_PUB_KEY'),
+        'client_secret': 'test_client_secret',
+        # 'client_secret': os.environ.get('STRIPE_SECTET_KEY'),
     }
 
     return render(request, template, context)
