0ac68ada058ab454ccb4db0e8f0b00902b216d35
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-22 16:07:09 +0000

    feat: Add webhooks handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,17 @@
+# Followed Code Institute's Boutique Ado guidance.
+
+from django.http import HttpResponse
+
+class StripWebHookHandler:
+    """ Handle Stripe Webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
