a4604eff64b8686aeaa70e7318add2d48be061b7
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-24 13:13:19 +0000

    feat: Add product foem for product admin

---
+++ products/form.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Category, Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        display_names = [(c.id, c.get_display_name()) for c in categories]
+
+        self.fields['category'].choices = display_names
+        for field_name, field in self.display_names:
+            field.widget.attrs['class'] = 'product-admin-inputs'
