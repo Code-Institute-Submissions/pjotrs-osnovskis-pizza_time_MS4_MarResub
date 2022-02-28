2fc48b3e6d6fef7d1a564261a094f3ac6d7909bb
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-02-28 20:13:01 +0000

    fix: Fix issue with no email connection, update settings.py

--- pizzatime/settings.py
+++ pizzatime/settings.py
@@ -227,8 +227,8 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_SMTP = 'smtp.gmail.com'
     EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
