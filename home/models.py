538661cf97978e029d86901a7c207f7f448958e2
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-03-03 19:37:19 +0000

    feat: Add custom address model for easy change if needed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    class Meta:
+        """ Spelling correction for admin page """
+        verbose_name_plural = 'Address'
+
+    street_name1 = models.CharField(max_length=254)
+    street_name2 = models.CharField(max_length=254, blank=True)
+    city = models.CharField(max_length=254)
+    postcode = models.CharField(max_length=254)
+    phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+            return self.street_name1 + ' ' + self.street_name2 
--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Address
 
 def index(request):
     """ A view to return an index page """
-    return render(request, 'home/pages/index.html')
+    address = Address.objects.all()
+    context = {
+        'address': address
+    }
+
+    return render(request, 'home/pages/index.html', context)
