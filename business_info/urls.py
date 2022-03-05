0bb64103d0145514987d1411a78b175e3cfc6ede
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2022-03-05 10:17:49 +0000

    feat: Add contact form and EmailJS

--- business_info/urls.py
+++ business_info/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.info, name='info'),
+    path('contact/', views.contact_us, name='contact_us'),
 ]
--- business_info/views.py
+++ business_info/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from home.models import Address
 
+import os
+if os.path.exists("env.py"):
+    import env
+
+
 def info(request):
     """ A view to return an info page """
     address = Address.objects.all()
@@ -8,4 +13,16 @@ def info(request):
         'address': address
     }
 
-    return render(request, 'business_info/base.html', context)
+    return render(request, 'business_info/about.html', context)
+
+def contact_us(request):
+    """ A view to return an info page """
+    address = Address.objects.all()
+    email_js_access_token = os.environ.get('EMAILJS_API_KEY')
+    context = {
+        'email_js_access_token': email_js_access_token,
+        'address': address,
+    }
+
+
+    return render(request, 'business_info/contact.html', context)
